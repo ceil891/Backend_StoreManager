c395ce7f224ba1a4ba12bf6b19d8b63a24679073
Luu Hung <luuhung261125@gmail.com>
2026-06-16 01:50:48 +0700

    Khởi đầu

---
+++ src/main/java/org/example/storemanager/StoremanagerApplication.java
@@ -0,0 +1,13 @@
+package org.example.storemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoremanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoremanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/storemanager/StoremanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.storemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoremanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
