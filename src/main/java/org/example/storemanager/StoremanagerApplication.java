be82ce08960aa6903d2d6eef5f9a6e50a89f2237
BNgoc005 <nguyenngoc081005@gmail.com>
2026-06-17 23:58:21 +0700

    Hoàn thiện tính năng Customer 1

--- src/main/java/org/example/storemanager/StoremanagerApplication.java
+++ src/main/java/org/example/storemanager/StoremanagerApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class gitStoremanagerApplication {
+public class StoremanagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StoremanagerApplication.class, args);
