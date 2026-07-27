165adef476c2819fedb195f0d17a2f71da564828
Luu Hung <luuhung261125@gmail.com>
2026-07-28 01:28:55 +0700

    V1

---
+++ src/main/java/org/example/storemanager/modules/common/controller/HealthController.java
@@ -0,0 +1,19 @@
+package org.example.storemanager.modules.common.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class HealthController {
+
+    @GetMapping({"/", "/health"})
+    public ResponseEntity<Map<String, String>> healthCheck() {
+        return ResponseEntity.ok(Map.of(
+                "status", "UP",
+                "service", "StoreManager Backend"
+        ));
+    }
+}
